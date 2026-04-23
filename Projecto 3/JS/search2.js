9b55d4a70efd375bd25134b92ee9f510dd0e0a4f
Ivan Bernabeu Romero <bernaveuromeroivan@fpllefia.com>
2026-04-23 11:18:05 +0200

    SEARCH

--- Projecto 3/JS/search2.js
+++ Projecto 3/JS/search2.js
@@ -1,17 +1,16 @@
 import { client } from "./data.js";
 
 async function cargarDatos() {
-  console.log("Cargando datos del formulario...");
-  
+
   const resForm = await client.from("form").select("*");
 
   if (resForm.data) {
     const formData = resForm.data;
     const formContenedor = document.querySelector("#formulario");
-    
+
     if (formContenedor) {
       formContenedor.innerHTML = "";
-      
+
       for (let i = 0; i < formData.length; i++) {
         formContenedor.innerHTML += `
           <div class="formulario">
@@ -31,6 +30,51 @@ async function cargarDatos() {
           </div>
         `;
       }
+    }
+  }
+  const resHoteles = await client.from("results").select("*");
+  const hotelesContenedor = document.querySelector("#hoteles");
+
+  if (resHoteles.data) {
+    const hotelesData = resHoteles.data;
+    hotelesContenedor.innerHTML = "";
+
+    for (let i = 0; i < hotelesData.length; i++) {
+      hotelesContenedor.innerHTML += `
+        <div class="contenido">
+          
+          <div class="imagen_contenido">
+            <img src="${hotelesData[i].imagen}">
+          </div>
+          
+          <div class="parte_central">
+            <h3>${hotelesData[i].name}</h3>
+            <div class="calificacion">
+              <img src="imagenes/estrellas.svg">
+              <span>${hotelesData[i].rating} (${hotelesData[i].reviewsCount} reviews)</span>
+            </div>
+            <div class="texto_dentro_recuadro">
+              <p>${hotelesData[i].description}</p>
+              <button>See availability</button>
+            </div>
+          </div>
+
+          <div class="parte_derecha_recuadro">
+            ${hotelesData[i].badge ? `<div class="parte_derecha_fondo_rojo">${hotelesData[i].badge}</div>` : ""}
+            
+            <div class="por_ciento">5% off</div>
+            
+            <p class="habitaciones">1 room 2 nights</p>
+            
+            <div class="precio">
+              <span class="subrayado">${hotelesData[i].oldPrice || ""}</span>
+              <span class="original"><strong>$${hotelesData[i].pricePerNight}</strong></span>
+            </div>
+            
+            <p class="include">Includes taxes and fees</p>
+          </div>
+
+        </div>`;
     }
   }
 }
